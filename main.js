96fcdb9ee35db0b3babf292dd628911ed10c2e03
XanderSimon <rednax01@gmail.com>
2023-01-30 15:49:14 -0500

    adding good coding practice

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 
-// initializing the count to start at 0 on page load
-count = 0;
+// Initializing the count to start at 0 on page load
+let count = 0;
 
-// function where the local variable count is updated and sent to the HTML object with the ID "number". This function is called by an onClick event for the button
+// Local variable count is updated and sent to the HTML object with the ID "number". This function is called by an onClick event for the button
 function addClick(){
   count++;
   document.getElementById("number").innerHTML = count;
