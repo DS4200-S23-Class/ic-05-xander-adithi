f3b16a5bf9d87876dd2f127892aacf473be89591
XanderSimon <rednax01@gmail.com>
2023-01-30 16:04:51 -0500

    change name of function

--- main.js
+++ main.js
@@ -3,7 +3,7 @@
 let count = 0;
 
 // Local variable count is updated and sent to the HTML object with the ID "number". This function is called by an onClick event for the button
-function addClick(){
+function buttonClicked(){
   count++;
   document.getElementById("number").innerHTML = count;
  }
