49cb48f4a0f8dcb6cb3f92a40b6b598a2804ba5b
XanderSimon <rednax01@gmail.com>
2023-01-30 15:47:36 -0500

    adding comments

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 
+// initializing the count to start at 0 on page load
 count = 0;
 
+// function where the local variable count is updated and sent to the HTML object with the ID "number". This function is called by an onClick event for the button
 function addClick(){
   count++;
   document.getElementById("number").innerHTML = count;
